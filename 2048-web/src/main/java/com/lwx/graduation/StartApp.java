a7457494d43050f5405db89184933236c9c0f595
03432935 <281969753@qq.com>
2022-01-05 21:01:26 +0800

    startapp

---
+++ 2048-web/src/main/java/com/lwx/graduation/StartApp.java
@@ -0,0 +1,12 @@
+package com.lwx.graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class StartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+}
